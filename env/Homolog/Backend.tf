25ef61687162873f1d2505a6a307418f1d977fc2
Greg Lixandrão <contato@greglixandrao.com>
2024-06-12 08:45:23 -0300

    change S3 bucket

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-alura"
+    bucket = "terraform-state-cicd-pipeline"
     key    = "Prod/terraform.tfstate"
     region = "us-west-2"
   }
