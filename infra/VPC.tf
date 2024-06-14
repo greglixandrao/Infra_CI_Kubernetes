e376ea5fbdad2f6a301a2bb0f8094c9b8870fa03
Greg Lixandrão <contato@greglixandrao.com>
2024-06-14 08:26:51 -0300

    add map_public_ip in VPC

--- infra/VPC.tf
+++ infra/VPC.tf
@@ -8,7 +8,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   database_subnets= ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  
+
   create_database_subnet_group           = true
   create_database_subnet_route_table     = true
   create_database_internet_gateway_route = true
@@ -17,4 +17,5 @@ module "vpc" {
   enable_dns_support   = true
 
   enable_nat_gateway = true
+  map_public_ip_on_launch = true
 }
